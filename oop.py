9d310392c9c4e2066d05124291c0b21bb20f16d5
Adva Haetzni <adva16@meet.mit.edu>
2015-01-25 19:51:18 +0200

    finished oop.py

--- oop.py
+++ oop.py
@@ -4,11 +4,30 @@ class animal:
 		self.age=age
 		self.color=color
 		self.size=size
+	def print_all(self):
+		print(self.name)
+		print(self.age)
+		print(self.color)
+		print(self.size)
 
+	def eating(self,food):
+		print("The Animal ", self.name, "is eating", food)
+
+	def sleeping(self,dream,hours):
+		print("the Animal",self.name,"is dreaming about", dream, "and sleeping for ", hours, "years")
 
 dog=animal("sadek",22,"yellow","tiny")
-print (dog.name)
+lion=animal("adva",2,"blue","big")
+dog.print_all()
+lion.print_all()
 
+
+a = input()
+dog.eating(a)
+
+b = input()
+c =input()
+dog.sleeping(b,c)
 
 
 
